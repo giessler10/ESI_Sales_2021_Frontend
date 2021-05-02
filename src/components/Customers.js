9965a632e931a709c672134d5358f1d9bc14f4f1
Katarina Helbig <ehelbig@stud.hs-offenburg.de>
2021-05-02 19:40:28 +0200

    Test Commit

---
+++ src/components/Customers.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { Button } from '@material-ui/core';
+
+const Customers = () => {
+    return (
+        <div>
+            <div />
+            <Typography paragraph>
+                Übersicht Kunden
+            </Typography>
+            <Typography paragraph>
+                Neuer Kunde1 anlegen!
+ 
+                <Button variant="contained" color="primary">
+                Neuer Kunde
+                </Button>
+            </Typography>
+        </div>
+    )
+}
+
+export default Customers
