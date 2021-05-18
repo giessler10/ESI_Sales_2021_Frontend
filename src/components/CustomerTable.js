f11abec18aa3d44258deda09f6b5500d3cd7dd34
Katarina Helbig <ehelbig@stud.hs-offenburg.de>
2021-05-18 23:20:47 +0200

    Kundenseite angepasst

--- src/components/CustomerTable.js
+++ src/components/CustomerTable.js
@@ -2,17 +2,96 @@ import React from 'react';
 import MUIDataTable from "mui-datatables";
 import {createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles'
 import FullScreenDialogCustomerDetails from'./FullScreenDialogCustomerDetails';
+import { useState, useEffect} from "react";
+import axios from "axios";
 
-//TesttabelleI Aufbau
-const columnsCustomers = ["Detailanzeige", "Customer No.", "Firstname", "Lastname", "Postcode ZIP", "Customer Type"];
-const dataCustomers = [
-  [<div><FullScreenDialogCustomerDetails/></div>, "1", "Lena", "Müller", "77865", "B"],
-  [<div><FullScreenDialogCustomerDetails/></div>,"2", "Marco", "Braun", "67889", "P"],
-  [<div><FullScreenDialogCustomerDetails/></div>,"3", "Siggy", "Franz", "11029", "P"],
-  [<div><FullScreenDialogCustomerDetails/></div>,"4", "Mira", "Graf", "77817", "P"],
-  [<div><FullScreenDialogCustomerDetails/></div>,"5", "Ludwig", "Steininger", "45467", "B"],
-];
-//TesttabelleI Aufbau Ende
+export default function CustomerTable(){
+
+  //Variables and constants  
+  const [selectedData, setSelectedData] =  useState([]); 
+  const [allData, setAllData] = useState([]); //alle Daten von DB.
+
+
+  //Columns with properties --> TODO auf eure Spaltennamen anpassen
+  const columns = [
+  {name: "C_NR", label: "Kunden-Nr", options: {filter: true, sort: true, display: true}}, 
+  {name: "C_CT_ID", label: "Kundenart-Nr", options: {filter: true, sort: true, display: false}}, 
+  {name: "C_COMPANY", label: "Firma", options: {filter: true, sort: false, display: true}},
+  {name: "C_FIRSTNAME", label: "Vorname",options: {filter: true,sort: false,display: true}},
+  {name: "C_LASTNAME",label: "Nachname",options: {filter: true,sort: false, display: true}},
+  {name: "C_CI_PC", label: "Postleitzahl", options: {filter: true,sort: true, display: true}},
+  {name: "CT_DESC", label: "Kundenart", options: {filter: true, sort: true, display: true}}];
+
+  const options = { onRowSelectionChange : (curRowSelected, allRowsSelected) => {rowSelectEvent(curRowSelected, allRowsSelected);},
+  customToolbarSelect: () => {return  <div><FullScreenDialogCustomerDetails/></div>;}
+};
+
+useEffect(() => {
+  // --> TODO  eurem REST Link einfügen
+  axios.get('https://hfmbwiwpid.execute-api.eu-central-1.amazonaws.com/sales/orders')
+      .then(res => {
+      console.log("RESPONSE:", res); //Data from Gateway
+      
+      if(IsDataBaseOffline(res)) return; //Check if db is available
+
+      if(res.data.length === 0) { //Check if data is available
+        setAllData(undefined);
+        return;
+      }          
+
+      if (DataAreEqual(allData, res.data)) return; //Check if data has changed       
+      setAllData(res.data); //Set new table data
+
+      })
+      .catch(err => {
+          console.log(err.message); //Error-Handling
+      })
+});
+
+  //Check if database is offline (AWS)
+  function IsDataBaseOffline(res){
+    if(res.data.errorMessage == null) return false; 
+    if(res.data.errorMessage === 'undefined') return false;
+    if(res.data.errorMessage.endsWith("timed out after 3.00 seconds")){
+        alert("Database is offline (AWS).");
+        return true;
+    }     
+    return false;
+  }
+
+    //Check if old data = new data
+    function DataAreEqual(data, sortedOrders){
+      if(data.sort().join(',') === sortedOrders.sort().join(',')){
+        return true;
+        }
+        else return false;
+      }
+
+//Get selected rows
+ function rowSelectEvent(curRowSelected, allRowsSelected){  
+
+  var _selectedData = [];
+
+  //No selection
+  if(allRowsSelected.length === 0) { 
+    setSelectedData(undefined);
+    return;
+  }
+
+  //Loop over all entries 
+  allRowsSelected.forEach(element => {
+    _selectedData.push(allData[element.dataIndex])
+  });
+ 
+  console.log("Selektierte Daten: ", _selectedData)
+  setSelectedData(_selectedData);
+  return;
+ }
+
+ //Lieferschein Button Click 
+ function OpenMore(){
+  <div><FullScreenDialogCustomerDetails/></div>
+ };
 
 const getMuiTheme = () => createMuiTheme({
   overrides: {
@@ -24,14 +103,17 @@ const getMuiTheme = () => createMuiTheme({
   }
 });
 
-const AllCustomers = () => {
   return (
-    <MuiThemeProvider theme={getMuiTheme()}> 
-    <MUIDataTable
-      data={dataCustomers}
-      columns={columnsCustomers}/>
+    <div>
+    <MuiThemeProvider theme={getMuiTheme()} > 
+      <MUIDataTable
+        data={allData}
+        columns={columns}
+        options={options}/>
+        <br></br>
     </MuiThemeProvider>
-  )
+
+   </div>
+
+  );            
 }
-            
-export default AllCustomers
--- src/components/customerOrdersTable.js
+++ src/components/customerOrdersTable.js
@@ -1,13 +1,10 @@
 import React, { useState, useEffect} from "react";
 import MUIDataTable from "mui-datatables";
 import {createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles';
-import {Button} from '@material-ui/core';
-import DescriptionIcon from '@material-ui/icons/Description';
 import axios from "axios";
-import jsPDFInvoiceTemplate, { OutputType, jsPDF } from "jspdf-invoice-template";
 
 
-export default function ShippingOrders(){
+export default function CustomerOrders(){
 
   //Variables and constants  
   const [selectedData, setSelectedData] =  useState([]); 
@@ -37,7 +34,7 @@ export default function ShippingOrders(){
   {name: "CT_DESC", label: "Kundenart", options: {filter: true, sort: true, display: true}}];
 
   const options = { onRowSelectionChange : (curRowSelected, allRowsSelected) => {rowSelectEvent(curRowSelected, allRowsSelected);},
-  customToolbarSelect: () => {return  <Button variant="contained" onClick={CreateDelivOrder}> <DescriptionIcon/>Lieferschein</Button>;}
+  customToolbarSelect: () => {}
 };
 
 useEffect(() => {
@@ -100,118 +97,6 @@ useEffect(() => {
   console.log("Selektierte Daten: ", _selectedData)
   setSelectedData(_selectedData);
   return;
- }
-
- //Lieferschein Button Click 
- function CreateDelivOrder(){
-
-  //Check, vor PDF-Druck, dass nur 1 Datensatz ausgewählt ist
-   if(selectedData.length > 1) {
-    alert("Bitte nur ein Datensatz auswählen");
-    return;
-  }
- 
-      // Abfrage Orderitems
-      axios.get('https://hfmbwiwpid.execute-api.eu-central-1.amazonaws.com/sales/orders/orderitems?O_NR=1')
-      .then(res => {
-      console.log("RESPONSE:", res); //Data from Gateway
-      
-      if(IsDataBaseOffline(res)) return; //Check if db is available
-
-      if(res.data.length === 0) { //Check if data is available
-        setAllData(undefined);
-        return;
-      }          
-
-      if (DataAreEqual(allData, res.data)) return; //Check if data has changed       
-      setAllData(res.data); //Set new table data
-
-      })
-      .catch(err => {
-          console.log(err.message); //Error-Handling
-      })
-    
-  var tableData = Array.from(Array(selectedData.length), (item, index)=>({
-    num: String(selectedData[index]["O_NR"]),
-    desc: String(selectedData[index]["O_NR"]),
-    price: String(selectedData[index]["O_NR"]),
-    quantity: String(selectedData[index]["O_NR"]),
-    unit: String(selectedData[index]["O_NR"]),
-    total: String(selectedData[index]["O_NR"])
-}));
-
-console.log("TableData", tableData);
-
-  var props = {
-    outputType: OutputType.Save,
-    returnJsPDFDocObject: true,
-    fileName: "Lieferschein",
-    orientationLandscape: false,
-    logo: {
-        src: "https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/logo.png",
-        width: 53.33, //aspect ratio = width/height
-        height: 26.66,
-        margin: {
-            top: 0, //negative or positive num, from the current position
-            left: 0 //negative or positive num, from the current position
-        }
-    },
-    business: {
-        name: "YourShirt GmbH",
-        address: "Schutterlindenberg 66, (DE) 77933 Lahr",
-        phone: "(+49) 7821 66 66 66",
-        email: "info@yourshirt.com",
-        //email_1: "info@example.al",
-        website: "www.yourshirt.de",
-    },
-    contact: {
-        label: "Invoice issued for:",
-        name: "Client Name",
-        address: "Albania, Tirane, Astir",
-        phone: "(+355) 069 22 22 222",
-        email: "client@website.al",
-        otherInfo: "www.website.al",
-    },
-    invoice: {
-        label: "Invoice #: ",
-        invTotalLabel: "Total:",
-        num: 19,
-        invDate: "Payment Date: 01/01/2021 18:12",
-        invGenDate: "Invoice Date: 02/02/2021 10:17",
-        header: ["#", "Description", "Price", "Quantity", "Unit", "Unit1"],
-        headerBorder: false,
-        tableBodyBorder: false,
-        table: tableData,
-        invTotal: "145,250.50",
-        invCurrency: "ALL",
-        row1: {
-            col1: 'VAT:',
-            col2: '20',
-            col3: '%',
-            style: {
-                fontSize: 10 //optional, default 12
-            }
-        },
-        row2: {
-            col1: 'SubTotal:',
-            col2: '116,199.90',
-            col3: 'ALL',
-            style: {
-                fontSize: 10 //optional, default 12
-            }
-        },
-        invDescLabel: "Invoice Note",
-        invDesc: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.",
-    },
-    footer: {
-        text: "The invoice is created on a computer and is valid without the signature and stamp.",
-    },
-    pageEnable: true,
-    pageLabel: "Page ",
-};
-
- const pdfObject = jsPDFInvoiceTemplate(props);
-
  }
 
 const getMuiTheme = () => createMuiTheme({
