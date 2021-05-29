67a5e43f24a3a40a03ef41bdc0e9c1be98d82f79
Tobias Giessler <tgiessle@stud.hs-offenburg.de>
2021-05-29 17:07:43 +0200

    add Lambda updateOrderState

--- src/components/ProductionButton.js
+++ src/components/ProductionButton.js
@@ -6,8 +6,9 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import PlayCircleFilledWhiteIcon from '@material-ui/icons/PlayCircleFilledWhite';
+import axios from "axios";
 
-export default function AlertDialog() {
+export default function AlertDialog(props) {
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -16,6 +17,40 @@ export default function AlertDialog() {
 
   const handleClose = () => {
     setOpen(false);
+
+    //Auftrag an Produktion übergeben
+    axios.get('https://hfmbwiwpid.execute-api.eu-central-1.amazonaws.com/sales/orders/' + props.O_NR + '?status=1')
+    .then(
+        (res) => {
+            console.log(res.status);
+            return res.data;
+        }
+    )
+    .then(
+        (res) => {
+          return res.body;
+        }
+    )
+    .catch(
+        (error) => {
+          console.log(error);
+          /*
+            
+            var errorObject = error.response.data;
+            var errorMessage = errorObject.errorMessage;
+            this.setState({ 
+                errorObject: errorObject,
+                errorMessage: errorMessage 
+            });
+            this.setState({ errorMessageVisible: true},()=>{ 
+                    window.setTimeout(()=>{
+                        this.setState({errorMessageVisible: false})
+                    },5000);
+                }
+            )
+          */
+        }
+    );
   };
 
   return (
