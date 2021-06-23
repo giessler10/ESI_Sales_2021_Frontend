7b762271e9ceb428d28a78510d77b42c1082da66
Tobias Giessler <tgiessle@stud.hs-offenburg.de>
2021-06-23 11:12:23 +0200

    Bugfixing | Dashbaord

--- src/components/DashboardDepositsSumReturnQTY.js
+++ src/components/DashboardDepositsSumReturnQTY.js
@@ -25,6 +25,8 @@ export default function DashboardDepositsSumReturnQTY(props) {
     var precentData = Math.round((props.data / props.sumQTY), 2);
     var precentData = (props.data / props.sumQTY) * 100;
     percent = precentData.toFixed(2) + "%";
+  }else{
+    percent = "0%";
   }
 
   return (
